3001632e5366894c2402e7438766760e2560b5a7
H4sh3 <danielpink@web.de>
2019-03-23 14:47:55 +0100

    add initial torch implementation

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 
 let objs = [];
 let cursor;
+let player;
+let torch;
 let centerLine;
 
 function setup() {
   createCanvas(1300, 900).parent('jsCanvas');;
   background(255, 112, 84);
   cursor = new Cursor();
-  objs.push(new Player())
+  player = new Player();
+  torch = new Torch(player.pos,cursor.pos);
+  objs.push(torch);
   objs.push(cursor);
+  objs.push(player);
   objs.push(new Block())
   objs.push(new Block())
 }
@@ -19,6 +24,36 @@ function draw() {
   objs.map(obj => obj.update());
 }
 
+class Torch{
+  constructor(){
+  }
+  
+  update(){
+    
+  }
+  
+  draw(){
+    fill(255, 245, 160)
+    noStroke()
+    let direction = cursor.pos.copy().sub(player.pos);
+    let tv1 = direction.copy().rotate(-HALF_PI/4).add(player.pos);
+    let tv2 = direction.copy().rotate(HALF_PI/4).add(player.pos)  
+    this.tL = createVector(tv1.x,tv1.y);
+    this.tR = createVector(tv2.x,tv2.y);
+    this.bL = createVector(0,15);
+    this.bR = createVector(0,-15);
+
+    this.bR.rotate(direction.heading())
+    this.bL.rotate(direction.heading())
+
+    this.bR.add(player.pos); 
+    this.bL.add(player.pos);
+    let torchHeadSize = createVector(mouseX,mouseY).dist(player.pos)
+    torchHeadSize*=0.82
+    ellipse(mouseX,mouseY,torchHeadSize,torchHeadSize)
+    quad(this.bL.x,this.bL.y,this.bR.x,this.bR.y,this.tL.x,this.tL.y,this.tR.x,this.tR.y)
+  }
+}
 
 class Player{
   constructor(){
@@ -32,12 +67,11 @@ class Player{
     if(this.bigDist()){
       this.handleAttraction();
       stroke(5)
-      line(this.pos.x,this.pos.y,mouseX,mouseY)
     }
   }
 
   bigDist(){
-    return this.pos.dist(cursor.pos) > 50;
+    return this.pos.dist(cursor.pos) > 150;
   }
   
   handleAttraction(){
